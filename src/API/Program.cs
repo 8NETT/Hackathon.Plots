c3fa1ddc1b300f70fb9048dc8828a52681628b40
Rodrigo Zanetta <rzssuchorzewski@outlook.com>
2026-02-27 18:16:08 -0300

    Removido o swagger em produção.

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -15,12 +15,17 @@ builder.Services.AddJwtAuthentication(builder.Configuration);
 var app = builder.Build();
 
 app.UseApiRequestLogging();
-app.UseSwagger();
-app.UseSwaggerUI(options =>
+
+if (app.Environment.IsDevelopment())
 {
-    options.RoutePrefix = string.Empty;
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-});
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
+}
+
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
